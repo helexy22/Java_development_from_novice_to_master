b677f91baf6620325aaefdc5667eb07bc35899b9
Jun Lee <lj288_learning@163.com>
2019-03-08 19:44:34 +0800

    Create 006_Syuan.java

---
+++ chapter3/006_Syuan.java
@@ -0,0 +1,11 @@
+
+public class Syuan {
+	public static void main(String args[])
+	{
+		double r=45.0324;
+		final double PI=3.1415926;
+		double area=PI*r*r;
+		System.out.println("圆的面积是：S="+area);
+	}
+
+}
