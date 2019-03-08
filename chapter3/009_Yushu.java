8525ee5dbf7fcd684831f520ec02dd3d4210c9bc
Jun Lee <lj288_learning@163.com>
2019-03-08 19:47:30 +0800

    Create 009_Yushu.java

---
+++ chapter3/009_Yushu.java
@@ -0,0 +1,18 @@
+public class Yushu {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int A=19%3;
+		int K=-19%-3;
+		int Q=19%-3;
+		int J=-19%3;
+		System.out.println("A=19%3的余数"+A);
+		System.out.println("K=-19%-3的余数"+K);
+		System.out.println("Q=19%-3的余数"+Q);
+		System.out.println("J=-19%3的余数"+J);
+	}
+
+	}
+
+
