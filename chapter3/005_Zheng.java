044884091096831e1e18c652fcf713a214f40e00
Jun Lee <lj288_learning@163.com>
2019-03-08 19:43:38 +0800

    Create 005_Zheng.java

---
+++ chapter3/005_Zheng.java
@@ -0,0 +1,18 @@
+public class zheng {
+	
+	public static void main(String args[])
+	{
+		
+        //正方形面积
+		int b=7;
+		int L=b*4;
+		int s=b*b;
+		System.out.println("正方形的周长为"+L);
+		System.out.println("正方形的面积为"+s);
+		//三角形面积
+		int a3=5,b3=7;
+		int s3=a3*b3/2;
+		System.out.println("三角形的面积为"+s3);	
+	}	
+
+}
