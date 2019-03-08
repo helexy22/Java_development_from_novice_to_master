116fe6942772552589e3fcd417daa37327863f48
Jun Lee <lj288_learning@163.com>
2019-03-08 19:41:00 +0800

    Create 002_PassTest.java

---
+++ chapter3/002_PassTest.java
@@ -0,0 +1,22 @@
+public class PassTest
+{
+	public static void main(String args[])
+	{
+		  //三角形面积
+		int a3=12,b3=34;
+		int s3=a3*b3/2;
+		System.out.println("三角形的面积为"+s3);
+		//正方形面积
+		double a1=12.2;
+		double s1=a1*a1;
+		System.out.println("正方形的面积为"+s1);
+        //长方形面积
+		double a2=100.1,b2=200.3;
+		double s2=a2*b2;
+		System.out.println("长方形的面积为"+s2);
+      
+	}
+
+
+}
+
