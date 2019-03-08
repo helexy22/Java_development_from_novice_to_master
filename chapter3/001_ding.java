392cb94fe27016d92644cc3fe7c633ee2b2f21fc
Jun Lee <lj288_learning@163.com>
2019-03-08 19:38:56 +0800

    Create 001_ding.java

---
+++ chapter3/001_ding.java
@@ -0,0 +1,23 @@
+public class ding   
+{  
+  //定义一个全局常量PI   
+  public final double PI = 3.1415926;
+  public final int aa = 24;
+  public final int bb = 36;
+  public final int cc = 48;
+  public final int dd = 60;
+  public String str1="hello";
+  public String str2="aa";
+  public String str3="bb";
+  public String str4="cc";
+  public String str5="dd";
+  public String str6="ee";
+  public String str7="ff";
+  public String str8="gg";
+  public String str9="hh";
+  public String str10="ii";
+  public Boolean mm=true;
+  public Boolean nn=false;
+}
+
+
