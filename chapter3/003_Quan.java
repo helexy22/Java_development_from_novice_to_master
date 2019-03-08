371225ad7e40ec01f77df53c494accd603130399
Jun Lee <lj288_learning@163.com>
2019-03-08 19:42:01 +0800

    Create 003_Quan.java

---
+++ chapter3/003_Quan.java
@@ -0,0 +1,26 @@
+public class Quan {
+	 byte x;
+	    short y;
+	    int z;
+	    int z1;
+	    long a;
+	    float b;
+	    double c;
+	    char d;
+	    boolean e;
+	    public static void main(String[] args)
+	    {
+	    	int z1=111;
+	        System.out.println(" 打印数据z="+z1);
+	        Quan m=new Quan();
+	        System.out.println(" 打印数据x="+m.x);
+	        System.out.println(" 打印数据y="+m.y);
+	        System.out.println(" 打印数据z="+m.z);
+	        System.out.println(" 打印数据a="+m.a);
+	        System.out.println(" 打印数据b="+m.b);
+	        System.out.println(" 打印数据c="+m.c);
+	        System.out.println(" 打印数据d="+m.d);
+	        System.out.println(" 打印数据e="+m.e);
+	    }
+
+}
